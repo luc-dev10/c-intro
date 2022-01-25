62a7397407e87edabd9250f1981e732bd4578d83
luc-dev10 <luc.dev1008@gmail.com>
2022-01-25 17:35:03 -0500

    comments/space

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello world");
+  
+  printf("newline, \n");
+  printf("\n");
+
+  printf("tab \t");
+  printf("\n");
+
+  printf("single quote \'");
+  printf("\n");
+
+  printf("double quote \"");
+  printf("\n");
+
+  printf("backslash \\");
+  printf("\n");
 }
