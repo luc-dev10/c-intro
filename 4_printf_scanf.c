28aa374ea5f23cbe98fa4cbb2de4619502a87130
luc-dev10 <luc.dev1008@gmail.com>
2022-01-26 18:27:03 -0500

    Create 4_printf_scanf.c

---
+++ 4_printf_scanf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  // %c	char
+  // %d	int
+  // %hd	short
+  // %ld	long
+  // %lld	long long
+  // %u	unsigned int
+  // %hu	unsigned short
+  // %lu	unsigned long
+  // %llu	unsigned long long
+  // %f	float
+  // %lf	double
+  // %s	string
+}
