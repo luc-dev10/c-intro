cc12973097e10aaa5466ec649bb72bc3058bc6be
luc-dev10 <luc.dev1008@gmail.com>
2022-01-26 18:26:59 -0500

    Create 3_numerical.c

---
+++ 3_numerical.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  // char  8 -128 to 127
+  // short 16 -32,768 to 32,767	
+  // long  32 -2,147,483,648 to 2,147,483,647	
+  // long long 64 -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807	
+  // int 32 -2,147,483,648 to 2,147,483,647
+
+  // float 32 -3.4x1038 to 3.4x1038
+  // double 64 -1.7x10308 to 1.7x10308
+
+  // unsigned char myVar;	 8 bits	0 to 255
+  // unsigned short myVar;	16 bits	0 to 65,535
+  // unsigned long myVar;	32 bits	0 to 4,294,967,295
+  // unsigned long long myVar;	64 bits	0 to 18,446,744,073,709,551,615	
+  // unsigned int myVar;	32 bits	0 to 4,294,967,295
+}
