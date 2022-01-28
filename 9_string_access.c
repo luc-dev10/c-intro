b0d08588645ae5deeb0ad87393ba1ee32d2baf09
luc-dev10 <luc.dev1008@gmail.com>
2022-01-28 16:52:50 -0500

    accessing string

---
+++ 9_string_access.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  char word[6] = "abcdef";
+  printf("%c", word[0]);
+  printf("%c", word[1]);
+  printf("%c", word[2]);
+  printf("%c", word[3]);
+  printf("%c", word[4]);
+  printf("%c", word[5]);
+}
