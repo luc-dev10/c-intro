518bc05a87115a6865698d801f80a48cc98fde2e
luc-dev10 <luc.dev1008@gmail.com>
2022-01-28 16:53:22 -0500

    overflow for integers

--- 2_int_overflow.c
+++ 2_int_overflow.c
@@ -4,7 +4,10 @@ int main(void) {
   
   // exceeding the stored value, will cut the number
   // value is around 2 billion
-  int i = 1000000000000;
-  printf("%d", i);
+  // int i = 1000000000000;
+  // printf("%d", i);
+  int x = 6, y = 2;
+  x /= y + 1;
+  printf("%d", x);
 
 }
