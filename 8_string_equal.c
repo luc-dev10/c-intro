7ab05a9ff7ef6138002bfe2cbf2b7b68539b6d3f
luc-dev10 <luc.dev1008@gmail.com>
2022-01-28 16:53:05 -0500

    equality method for string, == 0 for equality

---
+++ 8_string_equal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  
+  // return 0 if true
+  // return 1 if greater
+  // return -1 if not greater
+  int x = strcmp("hello", "hello");
+
+  if (x == 0)
+    printf("yes is true");
+}
