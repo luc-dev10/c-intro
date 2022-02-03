5b059b15bcba4a4f970ba4ff4c1f4ef4f9daba6a
luc-dev10 <luc.dev1008@gmail.com>
2022-02-02 21:14:50 -0500

    Create 16_str_method.c

---
+++ 16_str_method.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+  // nums
+  // cannot be changed values of array  
+
+  // returns 0 if equals
+  printf("%d\n", strcmp("he", "hello") == 0);
+
+  // length
+  printf("%d\n", strlen("hello"));
+
+  return 0;
+}
